29e91830a0787113ca84ba45aa1e70f1598028f7
heyeqiu <932343066@qq.com>
2023-01-11 17:47:12 +0800

    提交

---
+++ User.java
@@ -0,0 +1,22 @@
+package com.xrom.springboot.start.domain;
+
+import lombok.Data;
+
+import java.io.Serializable;
+
+/**
+ * 
+* @des
+* @author heyq  
+* @date 2021年5月26日  
+* @version 1.0
+ */
+@Data
+public class User implements Serializable{
+	private static final long serialVersionUID = 1L;
+	private String id;
+	private String username;
+	private String pwd;
+	private Integer isDelete;
+	private String img;
+}
